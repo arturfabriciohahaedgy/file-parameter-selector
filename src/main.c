0c4726f179bc3bd8d26a3a5fd3c7eb237917b3ef
arturfabriciohahaedgy <arturfabricio99@gmail.com>
2022-10-03 22:37:48 -0300

    moving initialization to main

--- src/main.c
+++ src/main.c
@@ -1,11 +1,28 @@
 #include <gtk/gtk.h>
-#include "files.h"
 #include "window.h"
 
-int main(int argc, char *argv[]) {
+Window          window;
+GtkWidget      *textbox;
+GtkWidget      *notebook;
+GtkEntryBuffer *entrybuffer;
+
+int
+main(int argc, char *argv[])
+{
+    char           *gladepath;
+
     gtk_init(&argc, &argv);
-    int init = initialize();
-    if (init != 0) {
-	return 1;
-    }
+    gladepath = "/home/artur/Programming/c/github/file-parameter-selector/ui/window.glade"; /* path to xml builder file */
+    window.Bui = gtk_builder_new_from_file(gladepath); /* builds xml file */
+    window.Win = GTK_WIDGET(gtk_builder_get_object(window.Bui, "window"));
+    window.ParenWin = GTK_WINDOW(window.Win); /* gets window itself */
+    gtk_window_set_title(window.ParenWin, "FPS");
+    g_signal_connect(window.Win, "destroy", G_CALLBACK(gtk_main_quit), NULL); /* makes application close it's resources after finishing window */
+    gtk_builder_connect_signals(window.Bui, NULL);
+    notebook = GTK_WIDGET(gtk_builder_get_object(window.Bui, "notebook"));
+    textbox = GTK_WIDGET(gtk_builder_get_object(window.Bui, "textbox"));
+    gtk_widget_show(window.Win); /* shows main window */
+    gtk_main(); /* makes GTK look for events */
+
+    return 0;
 }
