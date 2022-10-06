a92bad2bf6fa357749fee18e234291dd0a38949e
arturfabriciohahaedgy <arturfabricio99@gmail.com>
2022-10-06 14:33:46 -0300

    changed script name and added TODO reminder

--- configure.sh
+++ configure.sh
@@ -3,4 +3,9 @@
 # Gets fullpath of window.glade
 gladepath=$(realpath ./ui/window.glade)
 
+# OS that is running
+os = $(uname -a)
+
 sed -i "s|gladepath = \".*\"|gladepath = \"$gladepath\"|g" ./src/window.c
+
+# TODO: Make a case statement to test on other platforms
